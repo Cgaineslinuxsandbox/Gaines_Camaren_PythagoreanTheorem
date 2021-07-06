93de66fb34aea03cf932a49072f4f45b21d6fc69
Cam <camarengaineslinuxsandbox@gmail.com>
2021-07-06 17:50:34 -0600

    Fixed issue with VSCode, may need to add package for other IDE

--- Pythagorean.java
+++ Pythagorean.java
@@ -1,10 +1,8 @@
-package Gaines_Camaren_PythagoreanTheorem;
 public class Pythagorean {
     public double calculateHypotenuse() {
         int legA = 20;
         int legB = 60;
-        double SideC = Math.sqrt((legA^2)+(legB^2));
+        double SideC = Math.sqrt((legA * legA) + (legB * legB));
         return SideC;
     }
 }
-
