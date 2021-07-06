22c24ab2735c006e0b7f477f9f7aa725508c58b7
Cam <camarengaineslinuxsandbox@gmail.com>
2021-07-06 15:02:04 -0600

    need to have matt look at why it wont work

--- Pythagorean.java
+++ Pythagorean.java
@@ -1,10 +1,10 @@
 package Gaines_Camaren_PythagoreanTheorem;
 public class Pythagorean {
-    public double calculateHypotenuse(int legA, int legB) {
-        // the hypotenuse is the side across from the right angle. 
-        // calculate the value of c given legA and legB
-
-
+    public double calculateHypotenuse() {
+        int legA = 20;
+        int legB = 60;
+        double SideC = Math.sqrt((legA^2)+(legB^2));
+        return SideC;
     }
 }
 
--- PythagoreanTest.java
+++ PythagoreanTest.java
@@ -1,6 +1,8 @@
 package Gaines_Camaren_PythagoreanTheorem;
 public class PythagoreanTest{
     public static void main(String[] args){
-
+        Pythagorean pyth = new Pythagorean();
+        double SideC = pyth.calculateHypotenuse();
+        System.out.println("the length of side C is: " + SideC);
     }
 }
